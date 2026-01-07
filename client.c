1f171e22448a5f2e2897ef1ec9f4699de98c1daf
Ivan Chen <ichen60@stuy.edu>
2026-01-07 14:46:27 -0500

    cleaned up old code and comments

--- client.c
+++ client.c
@@ -14,32 +14,21 @@ void clientLogic(int server_socket){
       exit(1);
     }
     message[strcspn(message, "\n")] = '\0';
-
-    //int bytes_wrote = write(server_socket, message, strlen(message)); //stalls here
-    //if (bytes_wrote == -1) err(bytes_wrote, "In clientLogic: ");
     int send_code = send(server_socket, message, sizeof(message), 0);
     if (send_code == -1) err(send_code, "In ClientLogic");
-    //printf("ckpt in client sublogic");
-    //fflush(stdout);
 
-    //int bytes_read = read(server_socket, response, sizeof(response));
-    //if (bytes_read == -1) err(bytes_read, "in clientLogic: ");
     int recv_code = recv(server_socket, response, sizeof(response), 0);
     if (recv_code == 0){
-      printf("socket closed\n");
+      printf("server closed\n");
       fflush(stdout);
       exit(1);
     }
     if (recv_code == -1) err(recv_code, "In ClientLogic");
     printf("received: %s\n", response);
     fflush(stdout);
-    //sleep(1);
-    //read(from_server, &clear_read_buff, sizeof(int)); //clear pipe
     }
     close(server_socket);
 }
-
-
 
 int main(int argc, char *argv[] ) {
   char* IP = "127.0.0.1";
@@ -48,11 +37,5 @@ int main(int argc, char *argv[] ) {
   }
   int server_socket = client_tcp_handshake(IP); //WHY DOES THIS GO THROUGH BUT SERVER HANDHSKAE STALLLLL;LLLL
   if(server_socket == -1) err(server_socket, "In client: ");
-  //printf("client connected.\n");
-  //fflush(stdout);
   clientLogic(server_socket); //stalls in this call
-
-
-  //printf("server_socket_fd: %d\n", server_socket);
-  //fflush(stdout);
 }
--- server.c
+++ server.c
@@ -3,7 +3,6 @@
 
 char * rot13(char*s){
   int x = 13;
-
   for (int i = 0; i < strlen(s); i++){
     if (s[i] - 'a' < 26 && s[i] - 'a' >= 0){ //if diff in range then letter is lowercase
       if (s[i] + x > 'z'){
@@ -25,70 +24,38 @@ char * rot13(char*s){
   return s;
 }
 
-
-//void sig_handler(int signo){
-//  err(signo, "Caught signal: ");
-//  exit(1);
-//}
-
 void subserver_logic(int client_socket){
     int f = fork();
-    //printf("ckpt in subserver logic");
-    //fflush(stdout);
     if (f == 0){ //child
-      char message[256] = "";
-      char response[256] = "";
-
-
+      char message[256];
+      char response[256];
       while (1){
-        //int bytes_read = read(client_socket, message, sizeof(message));
-        //if (bytes_read == 0) {
-        //  err(client_socket, "In server sublogic: ");
-        //  exit(1);
-        //}
         int recv_code = recv(client_socket, message, sizeof(message), 0);
         if(recv_code == 0){
-          printf("socket closed\n");
+          printf("client exit\n");
           exit(1);
         }
         if (recv_code == -1) err(recv_code, "In subserver logic");
 
         //modify response with rot13
         strcpy(response, rot13(message));
-        //response[bytes_read] = '\0';
-        //int bytes_wrote = write(client_socket, response, sizeof(response));
-        //if (bytes_wrote == -1) {
-        //  err(bytes_wrote, "In server sublogic: ");
-        //  exit(1);
-        //}
-        //printf("%s\n", message);
         int send_code = send(client_socket, response, sizeof(response), 0);
         if (send_code == -1) err(send_code, "In subserver logic");
-
-        //signal(SIGINT, sig_handler);
       }
       exit(0);
     }
     else{
-      //printf("Ready for next client\n");
-      //fflush(stdout);
       close(client_socket);
-      }
+    }
 }
 
 int main(int argc, char *argv[] ) {
   int listen_socket = server_setup();
   while(1){
-    //printf("ckpt before server handshake\n"); //does not print but client still returns
-    //fflush(stdout);
     int client_socket = server_tcp_handshake(listen_socket); // stalls here
     if (client_socket == -1) err(client_socket, "In server main");
-    printf("successful server handshake\n");
+    printf("client connected\n");
     fflush(stdout);
-    //printf("client_socket_fd: %d\n", client_socket);
-    //fflush(stdout);
-
     subserver_logic(client_socket); //client_socket is server socket
-
   }
 }
