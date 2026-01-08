a46cb17ad70d3a6497940b0d02fee6c848c592da
Clayton <claytonzhu4@gmail.com>
2026-01-08 01:22:05 -0500

    Select after first recv makes no sense | Fixed select server not adding sockets

--- networking.c
+++ networking.c
@@ -9,6 +9,8 @@ int server_setup() {
   //setup structs for getaddrinfo
   struct addrinfo * hints, * results;//results is allocated in getaddrinfo
   hints = calloc(1,sizeof(struct addrinfo));
+  char * PORT = "9998";
+
   hints->ai_family = AF_INET;
   hints->ai_socktype = SOCK_STREAM; //TCP socket
   hints->ai_flags = AI_PASSIVE; //only needed on server
@@ -33,6 +35,41 @@ int server_setup() {
   //set socket to listen state
   err(listen(clientd, 10), "In Server setup - listen");
 
+  socklen_t sock_size;
+  struct sockaddr_storage client_address;
+  sock_size = sizeof(client_address);
+  fd_set read_fds;
+  char buff[1025]="";
+
+  while(1){
+    FD_ZERO(&read_fds);
+    FD_SET(STDIN_FILENO, &read_fds);
+    FD_SET(listen_socket,&read_fds);
+    int i = select(listen_socket+1, &read_fds, NULL, NULL, NULL);
+    //if standard in, use fgets
+    if (FD_ISSET(STDIN_FILENO, &read_fds)) {
+        fgets(buff, sizeof(buff), stdin);
+        buff[strlen(buff)-1]=0;
+        printf("Recieved from terminal: '%s'\n",buff);
+    }
+    // if socket
+    if (FD_ISSET(listen_socket, &read_fds)) {
+        //accept the connection
+        int client_socket = accept(listen_socket,(struct sockaddr *)&client_address, &sock_size);
+        printf("Connected, waiting for data.\n");
+        //read the whole buff
+        read(client_socket,buff, sizeof(buff));
+        //trim the string
+        buff[strlen(buff)-1]=0; //clear newline
+        if(buff[strlen(buff)-1]==13){
+            //clear windows line ending
+            buff[strlen(buff)-1]=0;
+        }
+        printf("\nRecieved from client '%s'\n",buff);
+        close(client_socket);
+    }
+  }
+  
   //free the structs used by getaddrinfo
   free(hints);
   freeaddrinfo(results);
