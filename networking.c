8671b3d1f33832659931c70a85bcef61bfe06aaf
Clayton <claytonzhu4@gmail.com>
2026-01-08 22:06:38 -0500

    Revert "changed var redeclaration"
    This reverts commit 73aa7b612c101e50045042f7cf315e192b7bbf7e.

--- networking.c
+++ networking.c
@@ -9,6 +9,7 @@ int server_setup() {
   //setup structs for getaddrinfo
   struct addrinfo * hints, * results;//results is allocated in getaddrinfo
   hints = calloc(1,sizeof(struct addrinfo));
+  char * PORT = "9998";
 
   hints->ai_family = AF_INET;
   hints->ai_socktype = SOCK_STREAM; //TCP socket
@@ -50,6 +51,12 @@ int server_setup() {
 int server_tcp_handshake(int listen_socket){
   int client_socket;
 
+  //socklen_t sock_size; //addrinfo
+  //struct sockaddr_storage client_address;
+  //sock_size = sizeof(client_address);    
+  //client_socket = accept(listen_socket,(struct sockaddr *)&client_address, &sock_size); //stalls here
+  //if (client_socket == -1) err(client_socket, "In Server tcp handshake");
+
   //select
   socklen_t sock_size;
   struct sockaddr_storage client_address;
@@ -71,8 +78,19 @@ int server_tcp_handshake(int listen_socket){
   if (FD_ISSET(listen_socket, &read_fds)) {
       //accept the connection
       int client_socket = accept(listen_socket,(struct sockaddr *)&client_address, &sock_size);
-      if (client_socket == -1) err(client_socket, "In Server tcp handshake"); 
       printf("Connected, waiting for data.\n");
+      
+      ////read the whole buff
+      //read(client_socket, buff, sizeof(buff));
+      ////trim the string
+      //buff[strlen(buff)-1]=0; //clear newline
+      //if(buff[strlen(buff)-1]==13){
+      //    //clear windows line ending
+      //    buff[strlen(buff)-1]=0;
+      //}
+//
+      //printf("\nRecieved from client '%s'\n",buff);
+      //close(client_socket);
   }
 
   return client_socket;
--- server.c
+++ server.c
@@ -1,58 +1,60 @@
 #include "networking.h"
 
 
+
 void server_logic(int client_socket){
-    char message[256];
-    char response[256];
+    //remove forking and child
+    int f = fork();
 
-    while (1){
-      int recv_code = recv(client_socket, message, sizeof(message), 0);
-      if(recv_code == 0){
-        printf("socket closed\n");
-        exit(1);
-      }
-      if (recv_code == -1) err(recv_code, "In subserver logic");
+    if (f == 0){ //child
+      char message[256] = "";
+      char response[256] = "";
 
-      char * token = strsep(&message, " ");
-      if (token == NULL) {
-        perror("Given empty string!");
-      }
-      else{ 
-        if (strcmp(token, "NAME ") == 0) { //how to store data?
+
+      while (1){
+
+        int recv_code = recv(client_socket, message, sizeof(message), 0);
+        if(recv_code == 0){
+          printf("socket closed\n");
+          exit(1);
+        }
+        if (recv_code == -1) err(recv_code, "In subserver logic");
+
+        char * token = strsep(&message, " ");
+        if (token == NULL) {
+          perror("Given empty string!");
+        }
+        else{ //come back for what to do with commands
+          if (strcmp(token, "NAME ") == 0) {
+            strcpy(response, message + 5);
+          } 
+          else if (strncmp(message, "MSG ", 4) == 0) {
+            strcpy(response, message + 4);
+          } 
+          else if (strncmp(message, "WHO", 3) == 0) {
+
+          } 
+          else if (strncmp(message, "QUIT", 4) == 0) {
+
+          } 
+          else strcpy(response, "Invalid command");
+
           printf("%s\n", message);
-          fflush(stdout);
-          //store user
-        } 
-        else if (strcmp(token, "MSG ") == 0) {
-          //print out in server
-          printf("%s\n", message);
-          fflush(stdout);
-          //send to other clients
-        } 
-        else if (strcmp(token, "WHO ") == 0) {
-          //list users
-          printf("%s\n", message);
-          fflush(stdout);
-        } 
-        else if (strcmp(token, "QUIT") == 0) {
-          //probably handle in client and kicks themselves
-          printf("%s\n", message);
-          fflush(stdout);
-        } 
-        else{
-          //err
-          printf("%s\n", message);
-          fflush(stdout);
+          int send_code = send(client_socket, response, sizeof(response), 0);
+          if (send_code == -1) err(send_code, "In subserver logic");
         }
       }
+      exit(0);
     }
-    close(client_socket); //not sure if this is correctly handled
+    else{
+      close(client_socket);
+      }
 }
 
 int main(int argc, char *argv[] ) {
   int listen_socket = server_setup();
   while(1){
-    listen_socket = server_tcp_handshake(listen_socket); // stalls here
+    int listen_socket = server_tcp_handshake(listen_socket); // stalls here
     if (listen_socket == -1) err(listen_socket, "In server main");
     printf("successful server handshake\n");
     fflush(stdout);
