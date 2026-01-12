09a5bfb4fa51087f87bc14f99e5cbbc1934eb804
Clayton <claytonzhu4@gmail.com>
2026-01-12 00:09:39 -0500

    loop through combined read and write set

--- server.c
+++ server.c
@@ -3,7 +3,7 @@
 #define NUMBER_OF_CLIENTS 100
 char names[NUMBER_OF_CLIENTS][256];
 
-void server_logic(int fd, char * message) {
+void server_logic(int fd, char * message, fd_set *master, int max_fd, int listen_socket) {
   char response[BUFFER_SIZE];
     
   if (strncmp(message, "NAME ", 5) == 0) {
@@ -12,15 +12,21 @@ void server_logic(int fd, char * message) {
   } else if (strncmp(message, "MSG ", 4) == 0) {
     snprintf(response, sizeof(response), "%s", message + 4);
   } else if (strcmp(message, "WHO") == 0) {
-    strncpy(response, "uhhh idk man this hard", sizeof(response));
+    strncpy(response, "uhhh idk man this hard", sizeof(response)); //?
   } else if (strcmp(message, "QUIT") == 0) {
     strncpy(response, "Quitting", sizeof(response));
     send(fd, response, sizeof(response), 0);
     return; // main loop handles close
   } else snprintf(response, sizeof(response), "Unknown command: %s", message);
 
-
-  send(fd, response, sizeof(response), 0);
+  //loop through all fd here
+  for (int i = 0; i <= max_fd; i++){
+    if (FD_ISSET(i, master)){
+      if (i != listen_socket && i != fd){
+        err(send(i, response, sizeof(response), 0) == -1);
+      }
+    }
+  }
 }
 
 int main(int argc, char *argv[] ) {
@@ -42,11 +48,11 @@ int main(int argc, char *argv[] ) {
     }
 
     for (int fd = 0; fd <= max_fd; fd++) {
-      if (FD_ISSET(fd, &read_fds)) {
+      if (FD_ISSET(fd, &read_fds)) { //fd is in there
         if (fd == listen_socket) {
           int client_fd = server_tcp_handshake(listen_socket);
           if (client_fd != -1) {
-            FD_SET(client_fd, &master);
+            FD_SET(client_fd, &master); //add fd to master
             if (client_fd > max_fd) max_fd = client_fd;
           }
         } else {
