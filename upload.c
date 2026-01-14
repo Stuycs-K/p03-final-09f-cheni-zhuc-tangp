b8a88d1afe4ddeb35a0ee03143011f448d6924aa
Clayton <claytonzhu4@gmail.com>
2026-01-14 13:21:20 -0500

    send_file done, recv_file almost, still needs testing

--- upload.c
+++ upload.c
@@ -1,5 +1,5 @@
-#include <sys/stats.h>
-#include <fcntl.h>
+#include "networking.h"
+
 
 int send_all(int socket, char * buffer, int length){
   //get file size using stat then keep sending until match
@@ -40,7 +40,9 @@ int recv_all(int socket, char * buffer, int length){
 }
 
 int send_file(int socket, char * filepath){
-  struct stat stat_buffer;
+  struct stat stat_buffer; 
+  char file_buffer[BUFFER_SIZE]; 
+  size_t n_read;
   err((stat(filepath, &stat_buffer) == -1), "in send_file");
 
   long file_size = stat_buffer.st_size;
@@ -53,12 +55,75 @@ int send_file(int socket, char * filepath){
 
   printf("Sending file: %s (%ld bytes)\n", filepath, file_size);
 
-  if (send_all(socket, , sizeof(file_size)) == -1) {
-    fclose(file);
-    return -1;
+  while(n_read = fread(file_buffer, 1, BUFFER_SIZE, file)) {
+    //while(total < n_read) 
+    if (n_read > 0){
+      size_t total = 0;
+      while (total < n_read){ //works bc buffer_size items, all of which are 1  
+        size_t bytes_sent = send(socket, file_buffer + total, n_read - total, 0);
+
+        if (bytes_sent < 0){
+          fclose(file);
+          perror("Send file error");
+          return -1;
+        }
+
+        if (bytes_sent == 0){
+          fclose(file);
+          return -1;
+        }
+
+        total += bytes_sent; 
+      }
+    }
+
+    if (n_read < sizeof(file_buffer)){ //end of file
+      break;
+    }
+
+  fclose(file);
+  return 0;
   }
 }
 
-int recieve_file(){
+int receive_file(int socket, char * filepath){
+  //get stat size from file
 
+  FILE *file = fopen(filepath, "wb");
+  if (!file) {
+    perror("fopen error");
+    return -1;
+  }
+
+  printf("Sending file: %s (%ld bytes)\n", filepath, file_size);
+
+  while(n_read = fread(file_buffer, 1, BUFFER_SIZE, file)) {
+    //while(total < n_read) 
+    if (n_read > 0){
+      size_t total = 0;
+      while (total < n_read){ //works bc buffer_size items, all of which are 1  
+        size_t bytes_sent = send(socket, file_buffer + total, n_read - total, 0);
+
+        if (bytes_sent < 0){
+          fclose(file);
+          perror("Send file error");
+          return -1;
+        }
+
+        if (bytes_sent == 0){
+          fclose(file);
+          return -1;
+        }
+
+        total += bytes_sent; 
+      }
+    }
+
+    if (n_read < sizeof(file_buffer)){ //end of file
+      break;
+    }
+
+  fclose(file);
+  return 0;
+  }
 }
