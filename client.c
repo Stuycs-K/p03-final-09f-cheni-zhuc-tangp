3d154b9c2d0aabda49efdd234f6d20d7d8000b74
Ivan Chen <ichen60@stuy.edu>
2026-01-14 14:50:58 -0500

    work on scrolling but still not working properly

--- client.c
+++ client.c
@@ -9,19 +9,24 @@ void clientLogic(int server_socket){
   WINDOW * textbox;
   initscr();
   wborder(stdscr, '|', '|', '-', '-', '+', '+', '+', '+');
-  // cbreak();
-  raw(); // prevents signals from being entered
+  cbreak();
+  // raw(); // prevents signals from being entered
   int row = 2;
   //noecho();
   refresh();
-  textbox = newwin(getmaxy(stdscr)-2, getmaxx(stdscr), 0, 0);
-  mvwprintw(textbox, 1, 1, "Connected to server. Type /help for commands.\n");
+  // scrollok(stdscr, TRUE);
+  scrollok(textbox, TRUE);
+  textbox = newwin(getmaxy(stdscr)-4, getmaxx(stdscr)-2, 1, 1);
+  wprintw(textbox, "Connected to server. Type /help for commands.\n");
   while(1){
     wborder(stdscr, '|', '|', '-', '-', '+', '+', '+', '+');
-    wborder(textbox, '|', '|', '-', '-', '+', '+', '+', '+');
+    mvwin(textbox, 1, 1); // doesn't actually move, but sends to the top
     for (int i = 1; i < getmaxx(stdscr)-1; i++){
+      mvprintw(getmaxy(stdscr)-3, i, "-");
       mvprintw(getmaxy(stdscr)-2, i, " ");
     }
+    mvprintw(getmaxy(stdscr)-3, 0, "+");
+    mvprintw(getmaxy(stdscr)-3, getmaxx(stdscr)-1, "+");
     refresh();
     wrefresh(textbox);
     move(getmaxy(stdscr)-2, 1);
@@ -39,7 +44,8 @@ void clientLogic(int server_socket){
       int recv_code = recv(server_socket, response, sizeof(response) - 1, 0);
       err(recv_code, "In cli logic");
       if (recv_code > 0) response[recv_code] = '\0';
-      mvwprintw(textbox, row++, 1, "%s\n", response); // fix later - increment row by amount of line breaks in response
+      wprintw(textbox, "%s\n", response);
+      // scroll(textbox);
       wrefresh(textbox);
     }
 
@@ -48,8 +54,7 @@ void clientLogic(int server_socket){
       message[strcspn(message, "\n")] = '\0';
       if (message[0] == '\0') continue;
       if (strncasecmp(message, "/help", 5) == 0){ // client side only, doesn't send to server
-        mvwprintw(textbox, row, 1, "/name [user] - change the name you are displayed as\n /who - display every user connected to the server\n /quit - exit from the server\n");
-        row+=3;
+        wprintw(textbox, "/name [user] - change the name you are displayed as\n/who - display every user connected to the server\n/quit - exit from the server\n");
         wrefresh(textbox);
       }else err(send(server_socket, message, strlen(message), 0), "In client logic");
 
