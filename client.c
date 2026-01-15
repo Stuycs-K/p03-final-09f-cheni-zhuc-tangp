2645b458544a22cedcd93a8d7fa0312bc62b89bf
Ivan Chen <ichen60@stuy.edu>
2026-01-15 14:31:04 -0500

    fixed syntax to reallow scrolling

--- client.c
+++ client.c
@@ -9,19 +9,23 @@ void clientLogic(int server_socket){
   WINDOW * textbox;
   initscr();
   wborder(stdscr, '|', '|', '-', '-', '+', '+', '+', '+');
-  // cbreak();
-  raw(); // prevents signals from being entered
+  cbreak();
+  // raw(); // prevents signals from being entered
   int row = 2;
   //noecho();
   refresh();
-  textbox = newwin(getmaxy(stdscr)-2, getmaxx(stdscr), 0, 0);
-  mvwprintw(textbox, 1, 1, "Connected to server. Type /help for commands.\n");
+  textbox = newwin(getmaxy(stdscr)-4, getmaxx(stdscr)-2, 1, 1);
+  scrollok(textbox,TRUE);
+  wprintw(textbox, "Connected to server. Type /help for commands.\n");
   while(1){
     wborder(stdscr, '|', '|', '-', '-', '+', '+', '+', '+');
-    wborder(textbox, '|', '|', '-', '-', '+', '+', '+', '+');
+    mvwin(textbox, 1, 1); // doesn't actually move, but sends to the top
     for (int i = 1; i < getmaxx(stdscr)-1; i++){
+      mvprintw(getmaxy(stdscr)-3, i, "-");
       mvprintw(getmaxy(stdscr)-2, i, " ");
     }
+    mvprintw(getmaxy(stdscr)-3, 0, "+");
+    mvprintw(getmaxy(stdscr)-3, getmaxx(stdscr)-1, "+");
     refresh();
     wrefresh(textbox);
     move(getmaxy(stdscr)-2, 1);
@@ -39,13 +43,13 @@ void clientLogic(int server_socket){
       int recv_code = recv(server_socket, response, sizeof(response) - 1, 0);
       err(recv_code, "In cli logic");
       if (recv_code == 0){
-        mvwprintw(textbox, row++, 1, "Server disconnected.\n");
+        wprintw(textbox, "Server disconnected.\n");
         wrefresh(textbox);
         endwin();
         exit(1);
       }
       if (recv_code > 0) response[recv_code] = '\0';
-      mvwprintw(textbox, row++, 1, "%s\n", response); // fix later - increment row by amount of line breaks in response
+      wprintw(textbox, "%s\n", response); // fix later - increment row by amount of line breaks in response
       wrefresh(textbox);
     }
 
@@ -59,12 +63,12 @@ void clientLogic(int server_socket){
 
       if (message[0] == '\0') continue;
       if (strncasecmp(message, "/help", 5) == 0) {
-        mvwprintw(textbox, row, 1, "/name [user] - change the name you are displayed as\n");
-        mvwprintw(textbox, row+1, 1, "/who - display every user connected to the server\n");
-        mvwprintw(textbox, row+2, 1, "/upload [filepath] - upload a file to the server\n");
-        mvwprintw(textbox, row+3, 1, "/list - view available files on server\n");
-        mvwprintw(textbox, row+4, 1, "/download [filename] - download a file from server\n");
-        mvwprintw(textbox, row+5, 1, "/quit - exit from the server\n");
+        wprintw(textbox, "/name [user] - change the name you are displayed as\n");
+        wprintw(textbox, "/who - display every user connected to the server\n");
+        wprintw(textbox, "/upload [filepath] - upload a file to the server\n");
+        wprintw(textbox, "/list - view available files on server\n");
+        wprintw(textbox, "/download [filename] - download a file from server\n");
+        wprintw(textbox, "/quit - exit from the server\n");
         row += 6;
         wrefresh(textbox);
       }
@@ -79,15 +83,15 @@ void clientLogic(int server_socket){
       else if ((strcasecmp(cmds[0], "/UPLOAD") == 0)){
         int argc = get_arr_len(cmds);
         if (argc != 2){ //wrong syntax
-          mvwprintw(textbox, row++, 1, "Error: Invalid syntax.\n");
-          mvwprintw(textbox, row++, 1, "Usage: /upload [filepath]\n");
+          wprintw(textbox, "Error: Invalid syntax.\n");
+          wprintw(textbox, "Usage: /upload [filepath]\n");
           wrefresh(textbox);
           continue;
         }
 
-        struct stat stat_buffer; 
+        struct stat stat_buffer;
         if (stat(cmds[1], &stat_buffer) == -1){
-          mvwprintw(textbox, row++, 1, "Error: File does not exist: %s\n", cmds[1]);
+          wprintw(textbox, "Error: File does not exist: %s\n", cmds[1]);
           wrefresh(textbox);
         }
 
@@ -95,7 +99,7 @@ void clientLogic(int server_socket){
         err(send(server_socket, message, strlen(message), 0), "In client logic"); //let server know file is coming
 
         char ack[256];
-        int ack_len = recv(server_socket, ack, sizeof(ack) - 1, 0); //wait for server ack so pure, blue-eyed, blonde files 
+        int ack_len = recv(server_socket, ack, sizeof(ack) - 1, 0); //wait for server ack so pure, blue-eyed, blonde files
         ack[ack_len] = '\0';
 
         send_file(server_socket, cmds[1]); //actual while loop sending
@@ -103,7 +107,7 @@ void clientLogic(int server_socket){
       else if ((strcasecmp(cmds[0], "/DOWNLOAD") == 0)){ //not implemented yet
         int argc = get_arr_len(cmds);
         if (argc != 2){ //wrong syntax
-          mvwprintw(textbox, row++, 1, "Usage: /download [filepath]\n");
+          wprintw(textbox, "Usage: /download [filepath]\n");
           wrefresh(textbox);
           continue;
         }
@@ -122,15 +126,15 @@ void clientLogic(int server_socket){
           char *filename = cmds2[1];
           long file_size = atol(cmds2[2]); //str t long
 
-          send(server_socket, "ACK\n", 4, 0);  
+          send(server_socket, "ACK\n", 4, 0);
 
           long received = receive_file(server_socket, filename, file_size);
 
           if (received == -1){
-            mvwprintw(textbox, row++, 1, "Error downloading file: %s\n", filename);
-          } 
+            wprintw(textbox, "Error downloading file: %s\n", filename);
+          }
           else {
-            mvwprintw(textbox, row++, 1, "File downloaded successfully: %s (%ld bytes)\n", filename, received);
+            wprintw(textbox, "File downloaded successfully: %s (%ld bytes)\n", filename, received);
           }
           wrefresh(textbox);
         }
